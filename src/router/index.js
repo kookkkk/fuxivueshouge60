f3e6fb2ba9f50c000f51b36e4e49eb429ff6722f
kookkkk <205892832@example.com>
2019-04-03 21:54:17 +0800

    登录路由配配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'hpme'
+  routes: [{
+    path: '/',
+    name: 'home'
 
-    }
-  ]
+  }, {
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })
